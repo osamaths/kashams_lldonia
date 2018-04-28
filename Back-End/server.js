0ef523b9262a06a3ed7a7ac2aa5e6cc102971612
Shahd Al-Jaouni <shahd.jaouni@gmail.com>
2018-04-28 22:34:44 +0300

    create multer

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,16 +1,16 @@
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var User= require('./User');
-var News=require('./News');
-var profile=require('./profile');
+var User= require('./Database/modules/User.js');
+var News=require('./Database/modules/News.js');
+var multer = require('multer');
+var Profile = require ('./routes/profile');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('./profile',profile);
+//app.use('/profile', Profile);
 
 
 var port = 3005;
@@ -21,6 +21,34 @@ mongoose.connect('mongodb://localhost:27017/kshamsDB', ()=>{
 
 var db = mongoose.connection;
 
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
+
+var upload = multer({ storage: storage }).single('profileImage');
+
+
+app.post('/profile', function (req, res) {
+  console.log ("inside /profile");
+    upload(req, res, function (err) {
+        if (err) {
+            // An error occurred when uploading
+
+        }
+
+        // Everything went fine
+    })
+});
+
+
+
 app.post('/user/signup', (req, res) => {
 	var newUser={
 		gender: req.body.gender,
@@ -28,13 +56,13 @@ app.post('/user/signup', (req, res) => {
 		email: req.body.email,
 		password:req.body.password,
 		year:req.body.year
-		
+
 	}
 
 	User.create(newUser, function(err, doc){
         if(err) return err;
         else { res.send(doc); }
-    }); 
+    });
 });
 
 
@@ -54,7 +82,7 @@ app.post('/user/login', (req, res) => {
 
 
 
- 
+
 app.listen(port, function() {
   console.log('app listening on port ' + port);
 });
