cf55ec58a6e81624f01552aaac52c29ddc987816
osamaths <osama.alshbikat@gmail.com>
2018-09-03 14:43:14 -0700

    linked react-native-vector-icons safely but still not working

--- Front-End/Components/TabNavigators/Menu.js
+++ Front-End/Components/TabNavigators/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 export default class Menu extends React.Component {
   static navigationOptions = {
     header: null
@@ -14,6 +15,7 @@ export default class Menu extends React.Component {
     return (
       <View>
         <Text>Profile Menu goes here.</Text>
+        <Icon name="facebook" size={30} color="#900" />
       </View>
     );
   }
