a90fac784209ccd48d7cff062bae6a634a59ea04
osamaths <osama.alshbikat@gmail.com>
2019-01-28 00:53:48 -0800

    Clear Branch Refactor

--- Front-End/Actions/miniHalqaActions.js
+++
@@ -1,6 +0,0 @@
-export const reserveMiniHalqa = halqa => {
-  console.log("Halqa #" + halqa._id + " has been reserved successfully.");
-  alert("Halqa " + halqa.name + " has been reserved successfully.");
-
-  // Reserve request goes here...
-};
